8b87757465d475c686258b4d2a82f71ec9dd5f29
DigiLuke <67799050+DigiLuke@users.noreply.github.com>
2025-12-23 22:38:26 +0100

    Add files via upload
    def for map height, width and that kind of stuff

---
+++ Gamedefs.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <cstdio>
+static constexpr int TILE_SIZE = 32;
+static constexpr int MAZE_WIDTH = 28;
+static constexpr int MAZE_HEIGHT = 31;
+static constexpr double PIXELS_PER_FRAME = 1.0;
